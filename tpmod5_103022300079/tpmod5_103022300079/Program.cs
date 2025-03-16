6fad876bc3fe8335b3412125906edb5f1ac64656
syahdan <syahdanmk1234@gmail.com>
2025-03-16 17:41:11 +0700

    Inisialisasi project tpmodul5_NIM

---
+++ tpmod5_103022300079/tpmod5_103022300079/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmod5_103022300079
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
