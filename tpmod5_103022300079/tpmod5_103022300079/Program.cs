9d21e078dc95cc8313afe5b846445aa9dacc4660
syahdan <syahdanmk1234@gmail.com>
2025-03-16 17:48:12 +0700

    Menambahkan class HaloGeneric dengan method SapaUser

--- tpmod5_103022300079/tpmod5_103022300079/Program.cs
+++ tpmod5_103022300079/tpmod5_103022300079/Program.cs
@@ -8,29 +8,11 @@ class HaloGeneric
     }
 }
 
-class DataGeneric<T>
-{
-    public T Data { get; }
-
-    public DataGeneric(T data)
-    {
-        Data = data;
-    }
-
-    public void PrintData()
-    {
-        Console.WriteLine($"Data yang tersimpan adalah: {Data}");
-    }
-}
-
 class Program
 {
     static void Main()
     {
         HaloGeneric halo = new HaloGeneric();
         halo.SapaUser("NamaPanggilan"); // Ganti dengan nama panggilan praktikan
-
-        DataGeneric<string> data = new DataGeneric<string>("103022300079"); // Ganti dengan NIM
-        data.PrintData();
     }
 }
